e127c036d4724922a96175fb360f2b64b1841fc5
Oleg Paslavskiy <bulochka.dmg@gmail.com>
2020-09-03 21:57:43 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,91 @@
+'use strict';
+
+// // s = 15;
+
+// // // переменные
+// // let number = 5;
+// // const age = 25;
+// // console.log(number);
+// // number = 10;
+// // console.log(number);
+
+// // var name = 'Oleg';
+
+// // Типы данных
+// let number = 4.6;
+// console.log(4/0);
+
+// const persone = `Oleg`;
+
+// const bool = true;
+// const bool2 = false;
+
+// const flex = {
+//     name: `oleg`,
+//     age: 25,
+//     city: `Kharkov`
+// };
+
+// // console.log(flex.age);
+// console.log(flex[`name`]);
+
+// const arr = [`hello`, `hi`, `priv`];
+// console.log(arr[1]);
+
+
+// alert(1);
+
+// const result = confirm('Privet, kak dela?');  // возвращает булиновое значение
+// console.log(result);
+
+// const res = prompt('Kak tebya zovut?', 'Oleg'); // возвращает только строку
+// console.log(typeof(res));
+
+// const answers = [];
+
+// // answers[0] = prompt('Как Ваше имя?', '');
+// // answers[1] = prompt('Как Ваша фамилия?', '');
+// // answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null)); // официально признаная ошибка которая выводит объект
+
+
+// Интерполяция
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}`);
+
+// const user = 'Ivan';
+// alert(`Hi, ${user}`);
+
+// Операторы в JS
+
+// Унарный + - это + который идет ПЕРЕД
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+
+// console.log(incr++); // Постфиксная форма декремента - возвращает сначала старое значение
+// console.log(decr--); // Постфиксная форма инкремента - возвращает сначала старое значение 
+
+// console.log(++incr); // Префиксная форма декремента - возвращает сразу новое значение
+// console.log(--decr); // Префиксная форма инкремента - возвращает сразу новое значение
+
+// console.log(5 % 2); // % - возвращает остаток
+
+// console.log(2 * 4 == '8');
+
+// // && - оператор И . работает когда оба варианта правдивы
+// // || - оператор ИЛИ. Работает когда один из вариантов правдив.
+
+// const isChecked = false,
+//     isClose = true;
+// console.log(isChecked || !isClose);
+
+// Работа с git и github
+
